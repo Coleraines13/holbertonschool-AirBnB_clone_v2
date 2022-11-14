55d965a4acd1b41df958f7011675cd1db8d15320
root <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>
2022-11-14 08:00:53 -0800

    i love my file of holding

---
+++ models/user.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""This module defines a class User"""
+from models.base_model import BaseModel
+
+
+<<<<<<< HEAD
+
+=======
+>>>>>>> parent of 95420d1... pushed user.py
+class User(BaseModel):
+    """This class defines a user by various attributes"""
+    email = ''
+    password = ''
+    first_name = ''
+    last_name = ''
