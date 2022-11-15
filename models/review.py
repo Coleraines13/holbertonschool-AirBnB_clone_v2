5aa7c903d6b0e993f728d52f68bae1a54b3b33ab
root <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>
2022-11-15 09:51:23 -0800

    fixedi think

--- models/review.py
+++ models/review.py
@@ -7,9 +7,9 @@ from sqlalchemy.orm import relationship
 from models.base_model import BaseModel, Base
 
 
-class Review(BaseModel):
+class Review(BaseModel, Base):
     """ Review classto store review information """
-    __tablename__ = 'review'
+    __tablename__ = 'reviews'
     place_id = Column(
             String(60), ForeignKey('places.id'), nullable=False
     ) if os.getenv('HBNB_TYPE_STORAGE') == 'db' else ''
