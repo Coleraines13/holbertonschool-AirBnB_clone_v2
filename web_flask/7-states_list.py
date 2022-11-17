465c0b03f11b712ae913c665abc779cc1e878e46
root <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>
2022-11-17 15:00:09 -0800

    doc again

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""this starts a script that starts a Flask web application"""
+"this starts a script that starts a Flask web application"
 
 from models import storage
 from flask import Flask, render_template
